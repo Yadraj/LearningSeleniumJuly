9ebd34fa6a39011ec4234eb6d4a1a7159b0948fe
raj <yadrajshinde1@gmail.com>
2025-07-19 23:44:38 +0530

    19th july

--- src/test/java/org/example/Selenium01.java
+++ src/test/java/org/example/Selenium01.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
 public class Selenium01 {
+
+    @Test
+    public void demo(){
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--incognito-mode");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.google.com/");
+        driver.quit();
+
+
+    }
 }
