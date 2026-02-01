188c2de68b4c3b4b50368a39ec3d99d5414c78a6
raj <yadrajshinde1@gmail.com>
2026-02-01 16:34:35 +0530

    Testng prioritizing changes

---
+++ src/test/java/org/example/TestNG/TestNG3.java
@@ -0,0 +1,27 @@
+package org.example.TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNG3 {
+
+    @Test
+    public void webcarloan(){
+
+        System.out.println("webcarloan");
+
+    }
+    @Test
+    public void mobilecarloan(){
+
+        System.out.println("mobilecarloan");
+    }
+
+    @Test
+    public void apicarloan(){
+
+        System.out.println("apicarloan");
+
+    }
+
+
+}
